4a24d15a3bd8cd72a461bd5588d28b09931f9de6
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2021-07-25 13:37:14 -0500

    Update elipse.py

--- elipse.py
+++ elipse.py
@@ -10,7 +10,7 @@ if not robot.Valid():
 reference = robot.Parent()  # devuelve el artículo robot.setPoseFrame(reference)#establece el marco de referencia de un robot pose_ref=robot.Pose()  #devuelve la posición actual del robot con matriz 
  
 def elipse(): 
-    Ref1 = RDK.Item("Target 2",ITEM_TYPE_TARGET) 
+    Ref1 = RDK.Item("Letras",ITEM_TYPE_TARGET) 
     RefTar = Ref1.Pose() 
     b1 = rob.pose_2_xyzrpw(RefTar)     
     t = -pi/2     
@@ -19,11 +19,11 @@ def elipse():
     z0= b1[2]     
     a=20     
     b=10 
-    RDK.RunProgram('WeldOff(-1)')     
-    robot.MoveJ(RefTar)     
+    RDK.RunProgram('WeldOn(-1)')     
+    robot.MoveJ(RefTar*roty(-pi))
     robot.setSpeed(2050,2000)    
     while (t <=3*pi/2):         
-        pos = transl(x0 + a*cos(t), y0 + b*sin(t), z0)*rotx(pi)*rotz(-pi/2)         
+        pos = transl(x0 + a*cos(t), y0 + b*sin(t), z0)*roty(-pi)         
         robot.MoveJ(pos)    
         if (t==-pi/2):       
             sleep(2) 
@@ -31,4 +31,4 @@ def elipse():
         t +=pi/360 
  
 elipse() 
-RDK.RunProgram('WeldOff(0)')    
+RDK.RunProgram('WeldOn(0)') 
