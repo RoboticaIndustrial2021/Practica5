fdc329ccf37fe96c71e5f31bdd32f4887d2ba27a
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2021-07-25 14:55:11 -0500

    Update parametrizables.py

--- parametrizables.py
+++ parametrizables.py
@@ -1,36 +1,37 @@
-from robolink import *    # RoboDK API      
-from robodk import*        # Robot toolbox
-
+from robolink import *    # RoboDK API 
+from robodk import *      # Robot toolbox 
+from time import*
 RDK = Robolink() 
-
-robot = RDK.ItemUserPick('',ITEM_TYPE_ROBOT) 	#adquirir todos los parámetros del robot 
+rob = robodk 
+ 
+robot = RDK.ItemUserPick('',ITEM_TYPE_ROBOT)    #adquirir todos los parámetros d el robot 
 if not robot.Valid(): 
     quit() 
-reference = robot.Parent()  # devuelve el artículo 
+reference = robot.Parent()  # devuelve el artículo robot.setPoseFrame(reference)#establece el marco de referencia de un robot pose_ref=robot.Pose()  #devuelve la posición actual del robot con matriz  
 robot.setPoseFrame(reference)#establece el marco de referencia de un robot 
-pose_ref=robot.Pose()  #devuelve la posición actual del robot con matriz 
-print (pose_ref)
-print("----")
-b=robodk.pose_2_xyzrpw(pose_ref) # devuelve la posición actual del robot 
-print("b = ",b) 
 
-frameletras = RDK.Item("Frame 2",ITEM_TYPE_FRAME) 
-robot.setPoseFrame(frameletras)
-
-tarletras = RDK.Item("letras",ITEM_TYPE_TARGET) 
-tarletras = tarletras.Pose() 
-
-for i in range(5): 
-    if i < 1:
-        robot.MoveJ(tarletras) 
-    robot.MoveL(tarletras*transl(-20*i,20,0))     
-     
-
-aprox = transl(0,0,-100) 
-retract = transl(0,0,-50) 
-for i in range(3): 
-    Refi = RDK.Item("Target "+str(i+2),ITEM_TYPE_TARGET) 
-    RefTar = Refi.Pose()     
-    robot.MoveJ(RefTar*aprox)     
-    robot.MoveL(RefTar)     
-    robot.MoveJ(RefTar*retract) 
+def elipse(): 
+    frameletras = RDK.Item("FrameLetras",ITEM_TYPE_FRAME) 
+    robot.setPoseFrame(frameletras)
+    Ref1 = RDK.Item("Letras",ITEM_TYPE_TARGET) 
+    RefTar = Ref1.Pose() 
+    b1 = rob.pose_2_xyzrpw(RefTar)     
+    t = -pi/2     
+    x0= b1[0]     
+    y0= b1[1]     
+    z0= b1[2]     
+    a=20     
+    b=10 
+    RDK.RunProgram('WeldOn(-1)')     
+    robot.MoveJ(RefTar*roty(pi))
+    robot.setSpeed(2050,2000)    
+    while (t <=3*pi/2):         
+        pos = RefTar*transl(a*cos(t),b*sin(t), 0)*roty(pi)
+        robot.MoveJ(pos)    
+        if (t==-pi/2):       
+            sleep(2) 
+            RDK.RunProgram('WeldOn')        
+        t +=pi/360 
+ 
+elipse() 
+RDK.RunProgram('WeldOn(0)') 
