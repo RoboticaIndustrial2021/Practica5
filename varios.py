91b9c5033f684972d759a75c00cc6cf3815e7086
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2021-07-25 16:49:24 -0500

    Update varios.py

--- varios.py
+++ varios.py
@@ -1,12 +1,13 @@
 from robolink import *    # RoboDK API
 from robodk import *      # Robot toolbox
+from time import*
 RDK = Robolink()
 
 
 # Program example:
 item1 = RDK.Item('box')
 item2 = RDK.Item('cil')
-frame1 = RDK.Item('Frame 3')
+frame3 = RDK.Item('Frame 3')
 
 
 if item1.Valid():
@@ -22,20 +23,21 @@ for i in itemlist:
  print(i.Name())
 
 item1.Copy()
-item1_Copy = frame1.Paste()
+item1_Copy = frame3.Paste()
 
 item2.Copy()
-item2_Copy = frame1.Paste()
-
+item2_Copy = frame3.Paste()
+sleep(2)
 martillo = RDK.MergeItems(list_items = [item1_Copy,item2_Copy])
 martillo.setVisible(False)
 posmartillo = martillo.Pose()
 martillo.setPose(posmartillo*transl(0,100,0))
 martillo.setName("martillo1_copy")
-martillo.Recolor([1,1,0,1])
+martillo.setVisible(True)
+sleep(2)
+martillo.Recolor([1,0,0,1])
 RDK.ShowMessage("solicitando a usuario... ",False)
 pedido = mbox("ingrese valor de escala en x,y,z desde 1 en adelante",entry="1,1,1")
 x,y,z = [int(x.replace(' ','')) for x in  pedido.split(',')]
 martillo.Scale([x,y,z])
-martillo.setVisible(True)
-RDK.ShowMessage("fin de programa ",False)
+RDK.ShowMessage("fin de programa ")
