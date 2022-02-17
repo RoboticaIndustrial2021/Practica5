e5011f3a5d2657037d071fda663672cbfbd02693
RoboticaIndustrial2021 <83782073+RoboticaIndustrial2021@users.noreply.github.com>
2022-02-17 13:06:15 -0500

    Update setparams.py

--- setparams.py
+++ setparams.py
@@ -3,30 +3,30 @@ from robodk import*        # Robot toolbox
 
 RDK = Robolink() 
 
-robot = RDK.ItemUserPick('',ITEM_TYPE_ROBOT) 	#adquirir todos los parámetros del robot 
+robot = RDK.Item('Kawasaki RS03N') 	#adquirir todos los parámetros del robot 
 if not robot.Valid(): 
     quit() 
 reference = robot.Parent()  # devuelve el artículo 
 robot.setPoseFrame(reference)#establece el marco de referencia de un robot 
 pose_ref=robot.Pose()  #devuelve la posición actual del robot con matriz
-b=robodk.pose_2_xyzrpw(pose_ref) # devuelve la posición actual del robot 
+b=Pose_2_TxyzRxyz(pose_ref) # devuelve la posición actual del robot 
 
-frameletras = RDK.Item("FrameLetras",ITEM_TYPE_FRAME) 
+frameletras = RDK.Item("FrameLetrasKawa",ITEM_TYPE_FRAME) 
 robot.setPoseFrame(frameletras)
 
-tarletras = RDK.Item("Letras",ITEM_TYPE_TARGET) 
+tarletras = RDK.Item("LetrasKawa",ITEM_TYPE_TARGET) 
 tarletras = tarletras.Pose()
 
 
 robot.setPoseFrame(frameletras)
 ORDEN1 = 0
-
-RDK.RunProgram("WeldOn(-1)")
+robot.setSpeed(10,10)
 RDK.setParam("orden1",ORDEN1)
 while (ORDEN1 < 3):
-    robot.MoveJ(tarletras*roty(-pi))
-    robot.MoveJ(tarletras*transl(0,50,0)*roty(-pi))
-    robot.MoveJ(tarletras*transl(100,0,50)*roty(-pi))
+    robot.MoveJ(tarletras)
+    robot.MoveJ(tarletras*transl(0,50,0))
+    robot.MoveJ(tarletras*transl(100,0,50))
     ORDEN1+= 1
+    RDK.ShowMessage("Valor de orden {}".format(ORDEN1),False)
     RDK.setParam("orden1",ORDEN1)
     
